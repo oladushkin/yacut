cd2a19e2311dce044900936feaa16867acc294b3
Dima Shelepin <oladushkin777@gmail.com>
2023-05-31 15:02:13 +0300

    добавление env

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ import os
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
     JSON_AS_ASCII = False

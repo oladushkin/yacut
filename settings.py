8d063e48a3838c05c6697f2e68f9eb9b1abee6d2
Dima Shelepin <oladushkin777@gmail.com>
2023-05-31 14:52:30 +0300

    ключ

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 class Config(object):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'YOUR_SECRET_KEY'
+    SECRET_KEY = '1234test4321'
     JSON_AS_ASCII = False

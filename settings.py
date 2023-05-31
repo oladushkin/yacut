eff5460749c497a13fdf87e89d57b52947c7a780
Dima Shelepin <oladushkin777@gmail.com>
2023-05-31 14:47:57 +0300

    подключение бд

--- settings.py
+++ settings.py
@@ -1,10 +1,5 @@
-# what_to_watch/settings.py
-
-import os
-
-
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = 'YOUR_SECRET_KEY'
     JSON_AS_ASCII = False
